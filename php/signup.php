b658b2ae563ed3e1b3fca8833365c90f46e71227
Jovannafinnalucita <enjopxdd@gmail.com>
2025-05-20 15:59:19 +0700

    signupdebug

--- php/signup.php
+++ php/signup.php
@@ -1,10 +1,11 @@
 <?php
-$username = $_POST["username"];
-$email = $_POST["email"];
-$password = $_POST["password"];
-
 require_once 'forum/koneksi.php';
-$sql="insert into user values(null,'$username','$password','$email',null,'member')";
+
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$sql="insert into user VALUES (null,'$username','$password','$email',null,'member')";
 mysqli_query($con,$sql);
 header('location:../html/home.html')
 ?>
