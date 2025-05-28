effdc7bad89157e884209405314bd79d00e5f176
garrychandra <garryalexanderchandra97@gmail.com>
2025-05-28 08:25:43 +0700

    update insertforum sql

--- insertforums.sql
+++ insertforums.sql
@@ -1,7 +1,7 @@
 DELETE FROM forum WHERE name LIKE 'Comments for%';
 
 
-INSERT INTO forum (name, created_at) VALUES
+INSERT INTO forum (name, date_created) VALUES
 ('Comments for Single: wave', NOW()),
 ('Comments for Single: light', NOW()),
 ('Comments for Single: surf.', NOW()),
@@ -11,11 +11,11 @@ INSERT INTO forum (name, created_at) VALUES
 ('Comments for Single: calla', NOW()),
 ('Comments for Single: dried flower', NOW());
 
-INSERT INTO forum (name, created_at) VALUES
+INSERT INTO forum (name, date_created) VALUES
 ('Comments for EP: wave 0.01', NOW()),
 ('Comments for EP: summer flows 0.02', NOW());
 
 
-INSERT INTO forum (name, created_at) VALUES
+INSERT INTO forum (name, date_created) VALUES
 ('Comments for Album: 0.1 flaws and all.', NOW()),
 ('Comments for Album: play with earth! 0.03', NOW());
